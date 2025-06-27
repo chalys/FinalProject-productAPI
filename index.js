f59162b64335b17c3f996445109a7eb577ed3e07
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2025-06-26 23:53:50 -0400

    feat: implement CORS and body-parser middleware

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 const PORT = 3000;
 app.listen(PORT, () => {
