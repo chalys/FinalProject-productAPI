5dba05e3a97b59b160e011e3b57c511fd8449351
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2025-07-04 00:19:05 -0400

    feat: add 404 handler middleware

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-
+import errorHandler from "./src/middlewares/error.middleware.js";
 const app = express();
 
 //Middlewares
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.urlencoded({extended:true}));
+
+//Routes
+//app.use('/api/products', (req, res)=>{res.send('API Rest en Node.js2')});
+app.get("/", (req, res) => {
+  res.send({ message: "API Rest en Node.js" });
+});
+
+// Error Handler
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+  console.error('Error', err.message); 
+  
+  if (res.headersSent) {
+    return next(err);
+  }
+  const statuse = err.statusCode || 500;
+  const message = err.message || 'Error interno del servidor';
+
+  res.status.json({error:true, message});
+};
+export default errorHandler;
